775cff0843f0899db03f0abcc1aa1ac4778f0503
rnikam59 <rnikam59@gmail.com>
2024-01-08 23:37:33 +0530

    Day 18 Testing code added with validation test case.

--- src/test/java/com/devbyteschool/blogs/controller/BlogControllerTest.java
+++ src/test/java/com/devbyteschool/blogs/controller/BlogControllerTest.java
@@ -7,14 +7,13 @@ import com.devbyteschool.blogs.service.BlogService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -22,26 +21,48 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
 
-@ExtendWith(SpringExtension.class)
+
+@ExtendWith(MockitoExtension.class)
 @WebMvcTest(value = BlogController.class)
 public class BlogControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private BlogService blogService;
-
     @Autowired
     private ObjectMapper objectMapper;
 
     @MockBean
+    private BlogService blogService;
+
+    @MockBean
     private BlogRepository blogRepository;
 
+    @Test
+    public void testUpdateBlogValidationFailed() throws Exception {
 
-    private String updateBlogSuccessRequest="";
+        UpdateBlogRequest updateBlogRequest = new UpdateBlogRequest();
+        updateBlogRequest.setBlogId("");
+        updateBlogRequest.setTitle("Spring Boot Blog Title.");
+        updateBlogRequest.setDescription("Spring Boot Blog Description.");
+        updateBlogRequest.setUserId("1001");
+        updateBlogRequest.setPublish(true);
 
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.put("/v1/blogs")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(updateBlogRequest))
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        MockHttpServletResponse response = result.getResponse();
+        assertEquals(HttpStatus.BAD_REQUEST.value(), response.getStatus());
+        assertEquals("{\"message\":\"BlogId is required parameter.\"}", response.getContentAsString());
+
+    }
 
     @Test
     public void testUpdateBlogRecordNotFound() throws Exception {
@@ -53,23 +74,17 @@ public class BlogControllerTest {
         updateBlogRequest.setUserId("1001");
         updateBlogRequest.setPublish(true);
 
-        Mockito.when(blogService.updateBlog(updateBlogRequest)).thenReturn(null);
+        when(blogService.updateBlog(any(UpdateBlogRequest.class))).thenReturn(null);
 
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.put("/v1/blogs")
+                .accept(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(updateBlogRequest))
+                .contentType(MediaType.APPLICATION_JSON);
 
-        try {
-            RequestBuilder requestBuilder = MockMvcRequestBuilders.put("/v1/blogs")
-                    .accept(MediaType.APPLICATION_JSON)
-                    .content(new ObjectMapper().writeValueAsString(updateBlogRequest))
-                    .contentType(MediaType.APPLICATION_JSON);
-
-            MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-            MockHttpServletResponse response = result.getResponse();
-            assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatus());
-            assertEquals("{\"message\":\"Record not present in database.\"}", response.getContentAsString());
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        MockHttpServletResponse response = result.getResponse();
+        assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatus());
+        assertEquals("{\"message\":\"Record not present in database.\"}", response.getContentAsString());
 
     }
 
@@ -89,9 +104,11 @@ public class BlogControllerTest {
         blog.setTitle("Spring Boot Blog Title.");
         blog.setDescription("Spring Boot Blog Description.");
         blog.setUserId("1001");
-        updateBlogRequest.setPublish(true);
+        blog.setPublish(true);
 
-        Mockito.when(blogService.updateBlog(updateBlogRequest)).thenReturn(blog);
+
+        when(blogService.updateBlog(any(UpdateBlogRequest.class))).thenReturn(blog);
+
 
         RequestBuilder requestBuilder = MockMvcRequestBuilders.put("/v1/blogs")
                 .accept(MediaType.APPLICATION_JSON)
